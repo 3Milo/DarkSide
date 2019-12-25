babe278232ab039918254e39eab80da3b333f6b7
Emil <emil@level7.systems>
2019-12-25 23:58:57 +0100

    block scrolling when menu open

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,13 @@ $('.burger').click(function() {
 	$('.menu').toggleClass('open');
 
 	$('html').css('overflow', $('.menu').hasClass('open') ? 'hidden' : 'visible');
+
+	if ($('.menu').hasClass('open')) {
+		setTimeout(() => {
+			$('.about, .offer, .portfolio, .prices, .contact, footer').css('display', 'none');
+		}, 400);
+	} else {
+		$('.about, .offer').css('display', 'flex');
+		$('.portfolio, .prices, .contact, footer').css('display', 'block');
+	}
 });
