f9defd4f792fd4f52400d693a7999bf170136625
Emil <emil@level7.systems>
2019-12-26 17:08:31 +0100

    add navbar animation, arrow and styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-$('.burger').click(function() {
-	$(this).toggleClass('open');
+$('.burger').click(() => {
+	$('.burger').toggleClass('open');
 	$('.menu').toggleClass('open');
 
 	$('html').css('overflow', $('.menu').hasClass('open') ? 'hidden' : 'visible');
@@ -11,5 +11,38 @@ $('.burger').click(function() {
 	} else {
 		$('.about, .offer').css('display', 'flex');
 		$('.portfolio, .prices, .contact, footer').css('display', 'block');
+	}
+});
+
+$('.options div').click(el => {
+	let text = el.target.textContent,
+		scroll;
+
+	if (text == 'O MNIE') {
+		scroll = $('.about');
+	} else if (text == 'OFERTA') {
+		scroll = $('.offer');
+	} else if (text == 'PORTFOLIO') {
+		scroll = $('.portfolio');
+	} else if (text == 'CENNIK') {
+		scroll = $('.prices');
+	} else {
+		scroll = $('.contact');
+	}
+
+	$("html, body").animate({ scrollTop: scroll.offset().top }, 100);
+});
+
+$('.arrow').click(el => {
+	$("html, body").animate({ scrollTop: 0 }, 100);
+});
+
+$(window).on('scroll', () => {
+	if ($(this).scrollTop() < 40) {
+		$('.navbar').addClass('show');
+		$('.arrow').removeClass('show');
+	} else {
+		$('.navbar').removeClass('show');
+		$('.arrow').addClass('show');
 	}
 });
